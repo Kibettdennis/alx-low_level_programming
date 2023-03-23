38003089121fe465eb2c2439c1a6e5f04f421aad
Kibettdennis <kibettkipronodennis@gmail.com>
2023-03-23 04:44:50 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,12 +1,16 @@
 #include "main.h"
+
 /**
  * mai - tests functions that prints if integer is positive or negative
  * Return: 0
  */
-int mai (void)
+int main (void)
+
 {
 int i;
+
 i = 0;
 positive_or_negative(i);
 return (0);
+
 }
