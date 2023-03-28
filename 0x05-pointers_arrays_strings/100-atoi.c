febbe4fb17ad94d771320badd3440b16c35c597f
Kibettdennis <kibettkipronodennis@gmail.com>
2023-03-27 19:18:16 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,7 +10,7 @@ int _atoi(char *s)
 {
 
 int c = 0;
-unsigned int ni = 0;
+unsigned int nil = 0;
 int min = 1;
 int isi = 0;
 
@@ -23,7 +23,7 @@ min *= -1;
 while (s[c] >= 48 && s[c] <= 57)
 {
 isi = 1;
-ni = (ni * 10) + (s[c] - '0');
+nil = (nil * 10) + (s[c] - '0');
 c++;
 }
 if (isi == 1)
@@ -32,7 +32,7 @@ break;
 }
 c++;
 }
-ni *= min;
+nil *= min;
 return (nil);
 }
 
